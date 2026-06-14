2d280bfdafab9e46a106e14dea1c33c39fc21d58
Andre Nicholas Jr <pumpgaming2@gmail.com>
2026-06-13 22:42:53 -0500

    Refactor VisualsSettingsSubState for improved structure

--- source/options/VisualsSettingsSubState.hx
+++ source/options/VisualsSettingsSubState.hx
@@ -1,297 +1,183 @@
 package options;
 
-import objects.Note;
-import objects.StrumNote;
-import objects.NoteSplash;
-import objects.Alphabet;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.text.FlxText;
+import flixel.math.FlxMath;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.util.FlxColor;
 
-class VisualsSettingsSubState extends BaseOptionsMenu
+import backend.ClientPrefs;
+import backend.Paths;
+
+class VisualsSettingsSubState extends flixel.FlxSubState
 {
-	var noteOptionID:Int = -1;
-	var notes:FlxTypedGroup<StrumNote>;
-	var splashes:FlxTypedGroup<NoteSplash>;
-	var noteY:Float = 90;
+	var options:Array<String> = [
+		'Hide HUD',
+		'Flashing Lights',
+		'Camera Zooms',
+		'Score Zoom',
+		'FPS Counter',
+		'Combo Stacking'
+	];
+
+	var vars:Array<String> = [
+		'hideHud',
+		'flashing',
+		'camZooms',
+		'scoreZoom',
+		'showFPS',
+		'comboStacking'
+	];
+
+	var descriptions:Array<String> = [
+		'Hides most HUD elements.',
+		'Disables flashing light effects.',
+		'Camera zooms on beat hits.',
+		'Score text grows when hitting notes.',
+		'Shows FPS counter.',
+		'Stacks combo and ratings for cleaner UI.'
+	];
+
+	var grp:FlxTypedGroup<FlxSprite>;
+	var txts:FlxTypedGroup<FlxText>;
+
+	var curSelected:Int = 0;
+
+	var title:FlxText;
+	var hint:FlxText;
+
 	public function new()
 	{
-		title = Language.getPhrase('visuals_menu', 'Visuals Settings');
-		rpcTitle = 'Visuals Settings Menu'; //for Discord Rich Presence
-
-		// for note skins and splash skins
-		notes = new FlxTypedGroup<StrumNote>();
-		splashes = new FlxTypedGroup<NoteSplash>();
-		for (i in 0...Note.colArray.length)
-		{
-			var note:StrumNote = new StrumNote(370 + (560 / Note.colArray.length) * i, -200, i, 0);
-			changeNoteSkin(note);
-			notes.add(note);
-			
-			var splash:NoteSplash = new NoteSplash(0, 0, NoteSplash.defaultNoteSplash + NoteSplash.getSplashSkinPostfix());
-			splash.inEditor = true;
-			splash.babyArrow = note;
-			splash.ID = i;
-			splash.kill();
-			splashes.add(splash);
-		}
-
-		// options
-		var noteSkins:Array<String> = Mods.mergeAllTextsNamed('images/noteSkins/list.txt');
-		if(noteSkins.length > 0)
-		{
-			if(!noteSkins.contains(ClientPrefs.data.noteSkin))
-				ClientPrefs.data.noteSkin = ClientPrefs.defaultData.noteSkin; //Reset to default if saved noteskin couldnt be found
-
-			noteSkins.insert(0, ClientPrefs.defaultData.noteSkin); //Default skin always comes first
-			var option:Option = new Option('Note Skins:',
-				"Select your prefered Note skin.",
-				'noteSkin',
-				STRING,
-				noteSkins);
-			addOption(option);
-			option.onChange = onChangeNoteSkin;
-			noteOptionID = optionsArray.length - 1;
-		}
-		
-		var noteSplashes:Array<String> = Mods.mergeAllTextsNamed('images/noteSplashes/list.txt');
-		if(noteSplashes.length > 0)
-		{
-			if(!noteSplashes.contains(ClientPrefs.data.splashSkin))
-				ClientPrefs.data.splashSkin = ClientPrefs.defaultData.splashSkin; //Reset to default if saved splashskin couldnt be found
-
-			noteSplashes.insert(0, ClientPrefs.defaultData.splashSkin); //Default skin always comes first
-			var option:Option = new Option('Note Splashes:',
-				"Select your prefered Note Splash variation.",
-				'splashSkin',
-				STRING,
-				noteSplashes);
-			addOption(option);
-			option.onChange = onChangeSplashSkin;
-		}
-
-		var option:Option = new Option('Note Splash Opacity',
-			'How much transparent should the Note Splashes be.',
-			'splashAlpha',
-			PERCENT);
-		option.scrollSpeed = 1.6;
-		option.minValue = 0.0;
-		option.maxValue = 1;
-		option.changeValue = 0.1;
-		option.decimals = 1;
-		addOption(option);
-		option.onChange = playNoteSplashes;
-
-		var option:Option = new Option('Hide HUD',
-			'If checked, hides most HUD elements.',
-			'hideHud',
-			BOOL);
-		addOption(option);
-		
-		var option:Option = new Option('Time Bar:',
-			"What should the Time Bar display?",
-			'timeBarType',
-			STRING,
-			['Time Left', 'Time Elapsed', 'Song Name', 'Disabled']);
-		addOption(option);
-
-		var option:Option = new Option('Flashing Lights',
-			"Uncheck this if you're sensitive to flashing lights!",
-			'flashing',
-			BOOL);
-		addOption(option);
-
-		var option:Option = new Option('Camera Zooms',
-			"If unchecked, the camera won't zoom in on a beat hit.",
-			'camZooms',
-			BOOL);
-		addOption(option);
-
-		var option:Option = new Option('Score Text Grow on Hit',
-			"If unchecked, disables the Score text growing\neverytime you hit a note.",
-			'scoreZoom',
-			BOOL);
-		addOption(option);
-
-		var option:Option = new Option('Health Bar Opacity',
-			'How much transparent should the health bar and icons be.',
-			'healthBarAlpha',
-			PERCENT);
-		option.scrollSpeed = 1.6;
-		option.minValue = 0.0;
-		option.maxValue = 1;
-		option.changeValue = 0.1;
-		option.decimals = 1;
-		addOption(option);
-		
-		#if !mobile
-		var option:Option = new Option('FPS Counter',
-			'If unchecked, hides FPS Counter.',
-			'showFPS',
-			BOOL);
-		addOption(option);
-		option.onChange = onChangeFPSCounter;
-		#end
-		
-		var option:Option = new Option('Pause Music:',
-			"What song do you prefer for the Pause Screen?",
-			'pauseMusic',
-			STRING,
-			['None', 'Tea Time', 'Breakfast', 'Breakfast (Pico)']);
-		addOption(option);
-		option.onChange = onChangePauseMusic;
-		
-		#if CHECK_FOR_UPDATES
-		var option:Option = new Option('Check for Updates',
-			'On Release builds, turn this on to check for updates when you start the game.',
-			'checkForUpdates',
-			BOOL);
-		addOption(option);
-		#end
-
-		#if DISCORD_ALLOWED
-		var option:Option = new Option('Discord Rich Presence',
-			"Uncheck this to prevent accidental leaks, it will hide the Application from your \"Playing\" box on Discord",
-			'discordRPC',
-			BOOL);
-		addOption(option);
-		#end
-
-		var option:Option = new Option('Combo Stacking',
-			"If unchecked, Ratings and Combo won't stack, saving on System Memory and making them easier to read",
-			'comboStacking',
-			BOOL);
-		addOption(option);
-
 		super();
-		add(notes);
-		add(splashes);
 	}
 
-	var notesShown:Bool = false;
-	override function changeSelection(change:Int = 0)
+	override function create()
 	{
-		super.changeSelection(change);
-		
-		switch(curOption.variable)
+		super.create();
+
+		// background
+		add(new FlxSprite().makeGraphic(FlxG.width, FlxG.height, 0xFF11151D));
+
+		title = new FlxText(50, 30, 600, "VISUALS SETTINGS", 24);
+		title.setFormat(Paths.font("vcr.ttf"), 24, FlxColor.WHITE);
+		add(title);
+
+		hint = new FlxText(50, 80, 900, "", 16);
+		hint.setFormat(Paths.font("vcr.ttf"), 16, FlxColor.WHITE);
+		add(hint);
+
+		grp = new FlxTypedGroup<FlxSprite>();
+		txts = new FlxTypedGroup<FlxText>();
+
+		add(grp);
+		add(txts);
+
+		var startY = 130;
+		var spacing = 55;
+
+		for (i in 0...options.length)
 		{
-			case 'noteSkin', 'splashSkin', 'splashAlpha':
-				if(!notesShown)
-				{
-					for (note in notes.members)
-					{
-						FlxTween.cancelTweensOf(note);
-						FlxTween.tween(note, {y: noteY}, Math.abs(note.y / (200 + noteY)) / 3, {ease: FlxEase.quadInOut});
-					}
-				}
-				notesShown = true;
-				if(curOption.variable.startsWith('splash') && Math.abs(notes.members[0].y - noteY) < 25) playNoteSplashes();
+			var bg = new FlxSprite(320, startY + i * spacing)
+				.makeGraphic(600, 45, 0xFF232A38);
+			bg.ID = i;
+			grp.add(bg);
+
+			var t = new FlxText(330, startY + i * spacing + 12, 600, "", 18);
+			t.setFormat(Paths.font("vcr.ttf"), 18, FlxColor.WHITE);
+			txts.add(t);
+		}
+
+		updateUI();
+	}
+
+	function getBool(i:Int):Bool
+	{
+		return Reflect.field(ClientPrefs.data, vars[i]);
+	}
+
+	function setBool(i:Int, value:Bool)
+	{
+		Reflect.setField(ClientPrefs.data, vars[i], value);
+	}
+
+	function getText(i:Int):String
+	{
+		return options[i].toUpperCase() + " : " + (getBool(i) ? "ON" : "OFF");
+	}
+
+	function toggle()
+	{
+		var newVal = !getBool(curSelected);
+		setBool(curSelected, newVal);
+
+		applySetting(vars[curSelected]);
+		ClientPrefs.saveSettings();
+
+		updateUI();
+	}
+
+	function applySetting(key:String)
+	{
+		switch (key)
+		{
+			case "showFPS":
+				if (Main.fpsVar != null)
+					Main.fpsVar.visible = ClientPrefs.data.showFPS;
+
+			case "camZooms":
+				// handled automatically by engine in most builds
 
 			default:
-				if(notesShown) 
-				{
-					for (note in notes.members)
-					{
-						FlxTween.cancelTweensOf(note);
-						FlxTween.tween(note, {y: -200}, Math.abs(note.y / (200 + noteY)) / 3, {ease: FlxEase.quadInOut});
-					}
-				}
-				notesShown = false;
+				// nothing
 		}
 	}
 
-	var changedMusic:Bool = false;
-	function onChangePauseMusic()
+	function updateUI()
 	{
-		if(ClientPrefs.data.pauseMusic == 'None')
-			FlxG.sound.music.volume = 0;
-		else
-			FlxG.sound.playMusic(Paths.music(Paths.formatToSongPath(ClientPrefs.data.pauseMusic)));
-
-		changedMusic = true;
-	}
-
-	function onChangeNoteSkin()
-	{
-		notes.forEachAlive(function(note:StrumNote) {
-			changeNoteSkin(note);
-			note.centerOffsets();
-			note.centerOrigin();
-		});
-	}
-
-	function changeNoteSkin(note:StrumNote)
-	{
-		var skin:String = Note.defaultNoteSkin;
-		var customSkin:String = skin + Note.getNoteSkinPostfix();
-		if(Paths.fileExists('images/$customSkin.png', IMAGE)) skin = customSkin;
-
-		note.texture = skin; //Load texture and anims
-		note.reloadNote();
-		note.playAnim('static');
-	}
-
-	function onChangeSplashSkin()
-	{
-		var skin:String = NoteSplash.defaultNoteSplash + NoteSplash.getSplashSkinPostfix();
-		for (splash in splashes)
-			splash.loadSplash(skin);
-
-		playNoteSplashes();
-	}
-
-	function playNoteSplashes()
-	{
-		var rand:Int = 0;
-		if (splashes.members[0] != null && splashes.members[0].maxAnims > 1)
-			rand = FlxG.random.int(0, splashes.members[0].maxAnims - 1); // For playing the same random animation on all 4 splashes
-
-		for (splash in splashes)
+		for (i in 0...grp.length)
 		{
-			splash.revive();
+			var bg = grp.members[i];
+			var t = txts.members[i];
 
-			splash.spawnSplashNote(0, 0, splash.ID, null, false);
-			if (splash.maxAnims > 1)
-				splash.noteData = splash.noteData % Note.colArray.length + (rand * Note.colArray.length);
+			if (bg == null || t == null) continue;
 
-			var anim:String = splash.playDefaultAnim();
-			var conf = splash.config.animations.get(anim);
-			var offsets:Array<Float> = [0, 0];
+			t.text = getText(i);
 
-			var minFps:Int = 22;
-			var maxFps:Int = 26;
-			if (conf != null)
+			if (i == curSelected)
 			{
-				offsets = conf.offsets;
-
-				minFps = conf.fps[0];
-				if (minFps < 0) minFps = 0;
-
-				maxFps = conf.fps[1];
-				if (maxFps < 0) maxFps = 0;
+				bg.color = 0xFF6D7A9A;
+				t.alpha = 1;
+				hint.text = descriptions[i];
 			}
-
-			splash.offset.set(10, 10);
-			if (offsets != null)
+			else
 			{
-				splash.offset.x += offsets[0];
-				splash.offset.y += offsets[1];
+				bg.color = 0xFF232A38;
+				t.alpha = 0.6;
 			}
-
-			if (splash.animation.curAnim != null)
-				splash.animation.curAnim.frameRate = FlxG.random.int(minFps, maxFps);
 		}
 	}
 
-	override function destroy()
+	function changeSelection(dir:Int)
 	{
-		if(changedMusic && !OptionsState.onPlayState) FlxG.sound.playMusic(Paths.music('freakyMenu'), 1, true);
-		Note.globalRgbShaders = [];
-		super.destroy();
+		curSelected = FlxMath.wrap(curSelected + dir, 0, options.length - 1);
+		updateUI();
 	}
 
-	#if !mobile
-	function onChangeFPSCounter()
+	override function update(elapsed:Float)
 	{
-		if(Main.fpsVar != null)
-			Main.fpsVar.visible = ClientPrefs.data.showFPS;
+		super.update(elapsed);
+
+		if (FlxG.keys.justPressed.UP)
+			changeSelection(-1);
+
+		if (FlxG.keys.justPressed.DOWN)
+			changeSelection(1);
+
+		if (FlxG.keys.justPressed.ENTER)
+			toggle();
+
+		if (FlxG.keys.justPressed.ESCAPE)
+			close();
 	}
-	#end
 }
