800d81281c5f7ea31f2e6be938f16058e85057b2
Andre Nicholas Jr <pumpgaming2@gmail.com>
2026-06-13 22:41:41 -0500

    Refactor GraphicsSettingsSubState for improved options

--- source/options/GraphicsSettingsSubState.hx
+++ source/options/GraphicsSettingsSubState.hx
@@ -1,97 +1,180 @@
 package options;
 
-import objects.Character;
+import flixel.FlxG;
+import flixel.FlxSubState;
+import flixel.FlxSprite;
+import flixel.text.FlxText;
+import flixel.math.FlxMath;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.util.FlxColor;
 
-class GraphicsSettingsSubState extends BaseOptionsMenu
+import backend.ClientPrefs;
+import backend.Language;
+import backend.Paths;
+
+class GraphicsSettingsSubState extends FlxSubState
 {
-	var antialiasingOption:Int;
-	var boyfriend:Character = null;
+	var options:Array<String> = [
+		'lowQuality',
+		'antialiasing',
+		'shaders',
+		'cacheOnGPU',
+		'framerate'
+	];
+
+	var optionNames:Array<String> = [
+		'Low Quality',
+		'Anti-Aliasing',
+		'Shaders',
+		'GPU Caching',
+		'Framerate'
+	];
+
+	var optionTypes:Array<String> = [
+		'bool',
+		'bool',
+		'bool',
+		'bool',
+		'int'
+	];
+
+	var optionDescriptions:Array<String> = [
+		'Reduces visual effects for better performance.',
+		'Smooths edges of graphics.',
+		'Enables visual effects.',
+		'Uses GPU memory for caching textures.',
+		'Controls FPS limit.'
+	];
+
+	var grpOptions:FlxTypedGroup<FlxSprite>;
+	var labels:FlxTypedGroup<FlxText>;
+
+	var curSelected:Int = 0;
+
+	var menuTitle:FlxText;
+	var menuHint:FlxText;
+
 	public function new()
 	{
-		title = Language.getPhrase('graphics_menu', 'Graphics Settings');
-		rpcTitle = 'Graphics Settings Menu'; //for Discord Rich Presence
-
-		boyfriend = new Character(840, 170, 'bf', true);
-		boyfriend.setGraphicSize(Std.int(boyfriend.width * 0.75));
-		boyfriend.updateHitbox();
-		boyfriend.dance();
-		boyfriend.animation.finishCallback = function (name:String) boyfriend.dance();
-		boyfriend.visible = false;
-
-		//I'd suggest using "Low Quality" as an example for making your own option since it is the simplest here
-		var option:Option = new Option('Low Quality', //Name
-			'If checked, disables some background details,\ndecreases loading times and improves performance.', //Description
-			'lowQuality', //Save data variable name
-			BOOL); //Variable type
-		addOption(option);
-
-		var option:Option = new Option('Anti-Aliasing',
-			'If unchecked, disables anti-aliasing, increases performance\nat the cost of sharper visuals.',
-			'antialiasing',
-			BOOL);
-		option.onChange = onChangeAntiAliasing; //Changing onChange is only needed if you want to make a special interaction after it changes the value
-		addOption(option);
-		antialiasingOption = optionsArray.length-1;
-
-		var option:Option = new Option('Shaders', //Name
-			"If unchecked, disables shaders.\nIt's used for some visual effects, and also CPU intensive for weaker PCs.", //Description
-			'shaders',
-			BOOL);
-		addOption(option);
-
-		var option:Option = new Option('GPU Caching', //Name
-			"If checked, allows the GPU to be used for caching textures, decreasing RAM usage.\nDon't turn this on if you have a shitty Graphics Card.", //Description
-			'cacheOnGPU',
-			BOOL);
-		addOption(option);
-
-		#if !html5 //Apparently other framerates isn't correctly supported on Browser? Probably it has some V-Sync shit enabled by default, idk
-		var option:Option = new Option('Framerate',
-			"Pretty self explanatory, isn't it?",
-			'framerate',
-			INT);
-		addOption(option);
-
-		final refreshRate:Int = FlxG.stage.application.window.displayMode.refreshRate;
-		option.minValue = 60;
-		option.maxValue = 240;
-		option.defaultValue = Std.int(FlxMath.bound(refreshRate, option.minValue, option.maxValue));
-		option.displayFormat = '%v FPS';
-		option.onChange = onChangeFramerate;
-		#end
-
 		super();
-		insert(1, boyfriend);
 	}
 
-	function onChangeAntiAliasing()
+	override function create()
 	{
-		for (sprite in members)
+		super.create();
+
+		// background
+		var bg = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, 0xFF11151D);
+		add(bg);
+
+		var panel = new FlxSprite(300, 70).makeGraphic(900, 460, 0xFF1D2433);
+		add(panel);
+
+		menuTitle = new FlxText(50, 30, 400, "GRAPHICS SETTINGS", 24);
+		menuTitle.setFormat(Paths.font("vcr.ttf"), 24, FlxColor.WHITE, LEFT);
+		add(menuTitle);
+
+		menuHint = new FlxText(50, 90, 500, "", 16);
+		menuHint.setFormat(Paths.font("vcr.ttf"), 16, FlxColor.WHITE, LEFT);
+		add(menuHint);
+
+		grpOptions = new FlxTypedGroup<FlxSprite>();
+		add(grpOptions);
+
+		labels = new FlxTypedGroup<FlxText>();
+		add(labels);
+
+		var startY:Float = 130;
+		var spacing:Float = 55;
+
+		for (i in 0...options.length)
 		{
-			var sprite:FlxSprite = cast sprite;
-			if(sprite != null && (sprite is FlxSprite) && !(sprite is FlxText)) {
-				sprite.antialiasing = ClientPrefs.data.antialiasing;
+			var box = new FlxSprite(320, startY + i * spacing).makeGraphic(600, 45, 0xFF232A38);
+			box.ID = i;
+			grpOptions.add(box);
+
+			var text = new FlxText(330, startY + i * spacing + 12, 600, "", 18);
+			text.setFormat(Paths.font("vcr.ttf"), 18, FlxColor.WHITE, LEFT);
+			labels.add(text);
+		}
+
+		changeSelection();
+	}
+
+	function getValueText(i:Int):String
+	{
+		var varName = options[i];
+
+		if (optionTypes[i] == "bool")
+		{
+			var val:Bool = Reflect.field(ClientPrefs.data, varName);
+			return optionNames[i].toUpperCase() + " : " + (val ? "ON" : "OFF");
+		}
+		else
+		{
+			var val:Int = Reflect.field(ClientPrefs.data, varName);
+			return optionNames[i].toUpperCase() + " : < " + val + " >";
+		}
+	}
+
+	function changeSelection(change:Int = 0)
+	{
+		curSelected = FlxMath.wrap(curSelected + change, 0, options.length - 1);
+
+		for (i in 0...grpOptions.length)
+		{
+			var box = grpOptions.members[i];
+			var text = labels.members[i];
+
+			text.text = getValueText(i);
+
+			if (i == curSelected)
+			{
+				box.color = 0xFF6D7A9A;
+				text.color = FlxColor.WHITE;
+				menuHint.text = optionDescriptions[i];
+			}
+			else
+			{
+				box.color = 0xFF232A38;
+				text.color = 0xFFAAAAAA;
 			}
 		}
 	}
 
-	function onChangeFramerate()
+	function toggleCurrent()
 	{
-		if(ClientPrefs.data.framerate > FlxG.drawFramerate)
+		var varName = options[curSelected];
+
+		if (optionTypes[curSelected] == "bool")
 		{
-			FlxG.updateFramerate = ClientPrefs.data.framerate;
-			FlxG.drawFramerate = ClientPrefs.data.framerate;
+			var current:Bool = Reflect.field(ClientPrefs.data, varName);
+			Reflect.setField(ClientPrefs.data, varName, !current);
 		}
 		else
 		{
-			FlxG.drawFramerate = ClientPrefs.data.framerate;
-			FlxG.updateFramerate = ClientPrefs.data.framerate;
+			var current:Int = Reflect.field(ClientPrefs.data, varName);
+			Reflect.setField(ClientPrefs.data, varName, current + 1);
 		}
 	}
 
-	override function changeSelection(change:Int = 0)
+	override function update(elapsed:Float)
 	{
-		super.changeSelection(change);
-		boyfriend.visible = (antialiasingOption == curSelected);
+		super.update(elapsed);
+
+		if (FlxG.keys.justPressed.UP)
+			changeSelection(-1);
+
+		if (FlxG.keys.justPressed.DOWN)
+			changeSelection(1);
+
+		if (FlxG.keys.justPressed.ENTER)
+		{
+			toggleCurrent();
+			changeSelection();
+		}
+
+		if (FlxG.keys.justPressed.ESCAPE)
+			close();
 	}
 }
